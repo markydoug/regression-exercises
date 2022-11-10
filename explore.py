253b58a97ae8fa69ad71e185df7b84f51a65e084
Mark Foster <markdouglasfoster@gmail.com>
2022-11-10 12:08:47 -0600

    Add functions to do univariate analysis

--- explore.py
+++ explore.py
@@ -3,6 +3,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+###################################################################################
+################################## EXPLORE DATA ###################################
+###################################################################################
+
+def explore_uvar(df, cont_vars, cat_vars):
+    explore_cont_uvar(df, cont_vars)
+    explore_cat_uvar(df, cat_vars)
+
+
+def explore_cont_uvar(df, cont_vars):
+    '''
+    Takes in a data frame and a list of continuous variables
+    Returns univarite stats for numerical data
+    '''
+    for col in cont_vars:
+        print(col)
+        print(df[col].describe())
+        df[col].hist()
+        plt.show()
+        sns.boxplot(y=col, data=df)
+        plt.show()
+
+def explore_cat_uvar(df, cat_vars):
+    '''
+    Takes in a data frame and a list of categorical variables
+    Returns univarite stats
+    '''
+    for col in cat_vars:
+        print(col)
+        print(df[col].value_counts())
+        print(df[col].value_counts(normalize=True)*100)
+        sns.countplot(x=col, data=df)
+        plt.show()
+
+def explore_bvar(df, target, cont_vars, cat_vars):
+    explore_cat_bvar(df, target, cont_vars)
+    explore_cont_bvar(df, target, cat_vars)
+
+def explore_cont_bvar(df, target, cont_vars):
+    '''
+    Takes in a data frame, target variable, and a 
+    list of continuos variables. Returns bivarite stats 
+    '''
+    for col in cont_vars:
+        sns.barplot(x=target, y=col, data=df)
+        rate = df[col].mean()
+        plt.axhline(rate,  label = f'Overall Mean of {col}', linestyle='dotted', color='black')
+        plt.legend()
+        plt.show()
+
+def explore_cat_bvar(df, target, cat_vars):
+    '''
+    Takes in a data frame, target variable, and a 
+    list of categorical variables. Returns bivarite stats 
+    '''
+    for col in cat_vars:
+        sns.barplot(x=col, y=target, data=df)
+        rate = df[target].mean()
+        plt.axhline(rate, label = f'Average {target} rate', linestyle='--', color='black')
+        plt.legend()
+        plt.show()
+
 def plot_variable_pairs(df, target):
     '''
     Takes in a dataframe and target variable and plots each feature 
